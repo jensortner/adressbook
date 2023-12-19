73f35315c74103f0add147b633707c0ec6498ce9
jens <jens.ortner@gmail.com>
2023-12-19 13:15:42 +0100

    Fist commit and push for adressbook project with mongoDB

---
+++ src/main/java/com/example/adressbook/Adress.java
@@ -0,0 +1,12 @@
+package com.example.adressbook;
+
+import lombok.Data;
+
+@Data
+public class Adress {
+    private static String streetName;
+    private static int streetNumber;
+    private static long zipCode;
+    private static String city;
+    private static String country;
+}
